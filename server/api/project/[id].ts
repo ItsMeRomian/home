61ced3aa1d28f324c2db45b493e8e09dd99bb46a
Romian Tairovski <romian.tairovski@gmail.com>
2024-04-22 16:48:14 +0200

    Add Github link to project actions

--- server/api/project/[id].ts
+++ server/api/project/[id].ts
@@ -105,7 +105,12 @@ export const projects = [
     ],
     summary:
       "<p>Welcome to the website of a journalist who writes about various topics and shares her insights with her readers. </p> <p class='uppercase font-bold'>How It Works</p> <p>This website is powered by Nuxt3, a framework that allows for server-side rendering (SSR) of web pages. This means that the pages are pre-rendered on the server and sent to the browser as HTML, which improves the performance, accessibility, and SEO of the website.</p> <p>The content of the website is stored in Firebase, a cloud platform that provides authentication, database, and storage services. Firebase Authentication handles the login and registration of users, Firebase Realtime Database stores the articles written by Annemiek, and Firebase Storage hosts the images and other media files.</p> <p>The website is deployed on Vercel, a cloud platform that specializes in hosting static and JAMstack websites. Vercel offers fast and reliable hosting, automatic SSL certificates, and continuous integration and delivery (CI/CD) workflows. This ensures that the website is always online and updated with the latest content.</p> <p class='uppercase font-bold'>Why It Matters</p> <p>This website is more than just a showcase of Annemiek\'s work. It is also a way for her to connect with her audience and share her opinions and perspectives on various issues. I've created a website that is easy to manage, update, and access for both Annemiek and her readers.</p> <p>If you are interested in creating a similar website for your own content, or if you have any questions or feedback, please feel free to contact me. I   would love to hear from you and help you with your web development needs. Thank you for reading! <br /><br /> <p>Here are some screenshots from the admin page:</p><img src='/imgs/journalistcrm-admin1.jpg'/><br/><img src='/imgs/journalistcrm-admin2.jpg'/>",
-    actions: [],
+    actions: [
+      {
+        name: "Github",
+        link: "https://github.com/itsmeromian/JournalistCMS",
+      }
+    ],
     image: "imgs/JournalistCRM.jpg",
   },
   {
